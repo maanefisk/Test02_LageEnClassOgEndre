44bff062aa91c0481edf866f3c89e7f2e230ed12
maanefisk <62015771+maanefisk@users.noreply.github.com>
2020-08-25 13:18:00 +0200

    Initial commit

---
+++ Ingrediens.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test02_LageEnClassOgEndre
+{
+    class Ingrediens
+    {
+        private int _amount;
+        private string _unit;
+
+        public Ingrediens(int amount, string unit)
+        {
+            _amount = amount;
+            _unit = unit;
+        }
+
+        public string GetDescription()
+        {
+            return _amount + " " + _unit;
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Test02_LageEnClassOgEndre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ingred = new Ingrediens(30, "dl");
+
+            Console.WriteLine(ingred.GetDescription());
+        }
+    }
+}
